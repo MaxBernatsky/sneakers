6d4f4d7cc89ab556b1a30fc6dc9ead234b7e1e28
Maxim Bernatsky <maxsateon@yandex.ru>
2023-06-02 17:15:18 +0300

    refactoring: create Card module scss

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import Header from './components/Header/Header';
 import Card from './components/Card/Card';
 import Drawer from './components/Drawer/Drawer';
 
+const arr = [
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 12999,
+    imgUrl: '/img/sneakers/1.jpg',
+  },
+  {
+    title: 'Мужские Кроссовки Nike Air Max 270',
+    price: 12999,
+    imgUrl: '/img/sneakers/2.jpg',
+  },
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 8499,
+    imgUrl: '/img/sneakers/3.jpg',
+  },
+  {
+    title: 'Кроссовки Puma X Aka Boku Future Rider',
+    price: 8999,
+    imgUrl: '/img/sneakers/4.jpg',
+  },
+];
+
 function App() {
   return (
     <div className='wrapper clear'>
@@ -16,10 +39,9 @@ function App() {
           </div>
         </div>
         <div className='d-flex'>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {arr.map((item) => (
+            <Card title={item.title} price={item.price} imgUrl={item.imgUrl} />
+          ))}
         </div>
       </div>
     </div>
